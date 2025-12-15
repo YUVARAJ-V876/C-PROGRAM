e28d8a57fcaab3a47fa98068fbb7074758389947
YUVARAJ-V876 <yuvarajvasantha876@gmail.com>
2025-12-15 16:14:27 +0530

    Delete code1.c

--- code1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-int myFavouriteNumber = 0;
-printf("please enter your favourite number\n");
-scanf("%d",&myFavouriteNumber);
-printf("you entered number: %d\n",myFavouriteNumber);
-return 0;
-}
